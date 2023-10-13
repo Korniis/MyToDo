f250cfe377b8275dd35973e48aa37c8fdc12ce12
Korniis <132537155+Korniis@users.noreply.github.com>
2023-10-13 23:14:01 +0800

    10/13

--- MyToDo/ViewModels/IndexViewModel.cs
+++ MyToDo/ViewModels/IndexViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using MyToDo.Common.Models;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyToDo.ViewModels
 {
-    internal class IndexViewModel
+    public class IndexViewModel : BindableBase
     {
+        public IndexViewModel()
+        {
+            TaskBars = new ObservableCollection<TaskBar> ();
+            CreatTaskBars();
+        }
+
+
+        private ObservableCollection<TaskBar> taskbars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskbars; }
+            set { taskbars = value; RaisePropertyChanged(); }
+        }
+        void CreatTaskBars() 
+        {
+
+            TaskBars.Add( new TaskBar() { Icon = "CalendarClock", Color="#ff0000ff ", Content="9" , Target="", Title=" 汇总"   });
+            TaskBars.Add( new TaskBar() { Icon = "AlarmCheck", Color="#ff1eca3a", Content="9" , Target="", Title="已完成"   });
+            TaskBars.Add( new TaskBar() { Icon = "ChartLineVariant", Color="#ff02c6dc", Content="100%" , Target="", Title="完成率"   });
+            TaskBars.Add( new TaskBar() { Icon = "PlaylistStar", Color= "#ffff0000", Content="9" , Target="", Title="备忘录"   });
+
+
+        }
+
     }
 }
