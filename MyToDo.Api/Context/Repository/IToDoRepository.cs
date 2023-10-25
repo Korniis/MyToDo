77b855817b0daa069e38234fb460b3d954f7f11e
Korniis <132537155+Korniis@users.noreply.github.com>
2023-10-26 00:47:46 +0800

    11

--- MyToDo.Api/Context/Repository/IToDoRepository.cs
+++ MyToDo.Api/Context/Repository/IToDoRepository.cs
@@ -4,35 +4,41 @@ namespace MyToDo.Api.Context.Repository
 {
     public interface IToDoRepository
     {
-        public Task<bool> Add(ToDo toDo);
-        public Task<bool> Update(ToDo toDo);
-        public Task<bool> Delete(int id);
+        Task<bool> Add(ToDo toDo);
+        Task<bool> Update(ToDo toDo);
+        Task<bool> Delete(int id);
 
 
 
+    }
+    public interface IMemoRepository
+    {
+
+        Task<Memo> Get(int id);
+        Task<Memo> Get(string id);
+            
     }
     public class ToDoRepository : IToDoRepository
     {
         private MyToDoContext doContext;
         public ToDoRepository(MyToDoContext doContext)
         {
-
             this.doContext = doContext;
         }
 
-        async Task<bool> Add(ToDo toDo)
+      public async Task<bool> Add(ToDo toDo)
         {
             await doContext.ToDo.AddAsync(toDo);
             return await doContext.SaveChangesAsync() > 0;
 
         }
 
-        Task<bool> Delete(int id)
+        public Task<bool> Delete(int id)
         {
             throw new NotImplementedException();
         }
 
-        Task<bool> Update(ToDo toDo)
+        public Task<bool> Update(ToDo toDo)
         {
             throw new NotImplementedException();
         }
