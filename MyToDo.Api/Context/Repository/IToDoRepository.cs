a4f8eb98396b9cd6244874970edb34fae9f6d202
Korniis <132537155+Korniis@users.noreply.github.com>
2023-10-26 00:47:26 +0800

    repository 实现

---
+++ MyToDo.Api/Context/Repository/IToDoRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyToDo.Api.Context.Repository
+{
+    public interface IToDoRepository
+    {
+        public Task<bool> Add(ToDo toDo);
+        public Task<bool> Update(ToDo toDo);
+        public Task<bool> Delete(int id);
+
+
+
+    }
+    public class ToDoRepository : IToDoRepository
+    {
+        private MyToDoContext doContext;
+        public ToDoRepository(MyToDoContext doContext)
+        {
+
+            this.doContext = doContext;
+        }
+
+        async Task<bool> Add(ToDo toDo)
+        {
+            await doContext.ToDo.AddAsync(toDo);
+            return await doContext.SaveChangesAsync() > 0;
+
+        }
+
+        Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> Update(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
