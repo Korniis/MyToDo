99c3a97e506961ade77ac921539d60b0ad207dbe
Korniis <132537155+Korniis@users.noreply.github.com>
2023-10-26 15:51:01 +0800

    类库添加

--- MyToDo.Api/Context/BaseEntiy.cs
+++ MyToDo.Api/Context/BaseEntiy.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public DateTime CreatDate { get; set; }
         public DateTime UpdateDate { get; set; }
+
     }
 }
--- MyToDo.Api/Context/ToDo.cs
+++ MyToDo.Api/Context/ToDo.cs
@@ -1,10 +1,9 @@
 ﻿namespace MyToDo.Api.Context
 {
-    public class ToDo:BaseEntity
+    public class ToDo : BaseEntity
     {
-
         public string Title { get; set; }
-    public string Content { get; set; }
-    public int Status { get; set; }
-}
+        public string Content { get; set; }
+        public int Status { get; set; }
+    }
 }
