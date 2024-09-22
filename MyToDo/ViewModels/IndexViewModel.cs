7a79407221bf25bc879feafe274e9ea0c1b46a15
Korniis <132537155+Korniis@users.noreply.github.com>
2024-09-22 12:56:26 +0800

    添加主页时间

--- MyToDo/ViewModels/IndexViewModel.cs
+++ MyToDo/ViewModels/IndexViewModel.cs
@@ -4,9 +4,11 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace MyToDo.ViewModels
 {
@@ -14,10 +16,49 @@ namespace MyToDo.ViewModels
     {
         public IndexViewModel()
         {
-            TaskBars = new ObservableCollection<TaskBar> ();
+            TaskBars = new ObservableCollection<TaskBar>();
+            InitTimer();
             CreatTaskBars();
             CreateTestDate();
         }
+
+        private void InitTimer()
+        {
+            DispatcherTimer _timer = new DispatcherTimer();
+            _timer.Interval = System.TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_tick;
+            _timer.Start();
+        }
+
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            DateNow = DateTime.Now.ToString("yyyy/MM/dd");
+            TimeNow = DateTime.Now.ToString("HH:mm:ss");
+            DayNow = DateTime.Now.ToString("dddd");
+        }
+        private string timeNow;
+
+        public string TimeNow
+        {
+            get { return timeNow; }
+            set { timeNow = value; RaisePropertyChanged(); }
+        }
+        private string dayNow;
+
+        public string DayNow
+        {
+            get { return dayNow; }
+            set { dayNow = value; RaisePropertyChanged(); }
+        }
+
+        private string dateNow;
+
+        public string DateNow
+        {
+            get { return dateNow; }
+            set { dateNow = value; RaisePropertyChanged(); }
+        }
+
 
 
         private ObservableCollection<TaskBar> taskbars;
@@ -42,27 +83,27 @@ namespace MyToDo.ViewModels
             set { memoDtos = value; RaisePropertyChanged(); }
         }
 
-        void CreatTaskBars() 
+        void CreatTaskBars()
         {
 
-            TaskBars.Add( new TaskBar() { Icon = "CalendarClock", Color="#ff0000ff ", Content="9" , Target="", Title=" 汇总"   });
-            TaskBars.Add( new TaskBar() { Icon = "AlarmCheck", Color="#ff1eca3a", Content="9" , Target="", Title="已完成"   });
-            TaskBars.Add( new TaskBar() { Icon = "ChartLineVariant", Color="#ff02c6dc", Content="100%" , Target="", Title="完成率"   });
-            TaskBars.Add( new TaskBar() { Icon = "PlaylistStar", Color= "#ffff0000", Content="9" , Target="", Title="备忘录"   });
+            TaskBars.Add(new TaskBar() { Icon = "CalendarClock", Color = "#ff0000ff ", Content = "9", Target = "", Title = " 汇总" });
+            TaskBars.Add(new TaskBar() { Icon = "AlarmCheck", Color = "#ff1eca3a", Content = "9", Target = "", Title = "已完成" });
+            TaskBars.Add(new TaskBar() { Icon = "ChartLineVariant", Color = "#ff02c6dc", Content = "100%", Target = "", Title = "完成率" });
+            TaskBars.Add(new TaskBar() { Icon = "PlaylistStar", Color = "#ffff0000", Content = "9", Target = "", Title = "备忘录" });
 
 
         }
-        void CreateTestDate() 
+        void CreateTestDate()
         {
-            ToDoDtos= new ObservableCollection<ToDoDto>();
+            ToDoDtos = new ObservableCollection<ToDoDto>();
             MemoDtos = new ObservableCollection<MemoDto>();
             for (int i = 0; i < 10; i++)
             {
-                ToDoDtos.Add(new ToDoDto() { Title="待办"+i , Content="正在处理中。。。。。" });
-                MemoDtos.Add(new MemoDto() { Title = "备忘"+i , Content = "我的密码" });
+                ToDoDtos.Add(new ToDoDto() { Title = "待办" + i, Content = "正在处理中。。。。。" });
+                MemoDtos.Add(new MemoDto() { Title = "备忘" + i, Content = "我的密码" });
 
             }
-        
+
         }
 
     }
